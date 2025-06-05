15a11641ecdd5d6360d40afc0175f3a43f8a0036
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-05 11:41:30 +0200

    Add initial serializers, views, and routes

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,44 @@
-# write serializers here
+from rest_framework import serializers
+
+from cinema.models import Movie, CinemaHall, Genre, Actor, MovieSession
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ["id", "name", "rows", "seats_in_row"]
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["id", "name"]
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ["id", "first_name", "last_name"]
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = [
+            "id",
+            "description",
+            "duration",
+            "genres",
+            "actors"
+        ]
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    movie = MovieSerializer(read_only=True)
+    cinema_hall = CinemaHallSerializer(read_only=True)
+
+    class Meta:
+        model = MovieSession
+        fields = ["id", "show_time", "movie", "cinema_hall"]
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,18 @@
-# write urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import MovieSessionViewSet, MovieViewSet, GenreViewSet, ActorViewSet, CinemaHallViewSet
+
+router = DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("movies_sessions", MovieSessionViewSet)
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+
+app_name = "cinema"
